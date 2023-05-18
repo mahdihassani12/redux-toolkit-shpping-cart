34df2f469206f339f804e62fd6b3976a57afbe3c
Mahdi <hassanim430@gmail.com>
2023-05-18 20:49:41 +0430

    adding not found component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Cart from './components/Cart';
 import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home></Home>} ></Route>
           <Route path='/Cart' element={<Cart></Cart>} ></Route>
+          <Route path='*' element={<NotFound></NotFound>} ></Route>
         </Routes>
     </div>
   );
---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,12 @@
+const NotFound = () => {
+    return (
+        <div className="container">
+            <div className="not-found">
+                <p>404</p>
+                <h1>Page Not Found</h1>
+            </div>
+        </div>
+    );
+}
+ 
+export default NotFound;
