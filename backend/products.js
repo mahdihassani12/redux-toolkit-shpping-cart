ca9ead6a8d0c25443898e1d82b6980241f09581d
Mahdi <hassanim430@gmail.com>
2023-05-20 11:41:07 +0430

    Adding products API and dummy products

---
+++ backend/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "IPhone 12 pro",
+    desc: "6.1-inch display",
+    price: 666,
+    image: "./assets/images/iphone12pro.jpg",
+  },
+  {
+    id: 2,
+    name: "IPhone 12",
+    desc: "5.4-inch display",
+    price: 600,
+    image: "./assets/images/iphone12.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "4K display resolation",
+    price: 669,
+    image: "./assets/images/galaxyS.jpg",
+  },
+  {
+    id: 4,
+    name: "Poco Fi",
+    desc: "Gaming CPU and Graphic",
+    price: 999,
+    image: "./assets/images/pocof1.jpg",
+  },
+];
+
+module.exports = products;
--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const products = require("./products");
+
 app.get("/", (req, res) => {
   res.send("Welcome to our online shop API");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
 app.listen(5000, () => {
