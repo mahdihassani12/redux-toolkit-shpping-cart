40c6ce0e6183c6c0c465cbd617e48d34af0c2acc
Mahdi <hassanim430@gmail.com>
2023-05-27 10:47:36 +0430

    adding styles to products

--- backend/products.js
+++ backend/products.js
@@ -4,28 +4,28 @@ const products = [
     name: "IPhone 12 pro",
     desc: "6.1-inch display",
     price: 666,
-    image: "./assets/images/iphone12pro.jpg",
+    image: "iphone12pro.jpg",
   },
   {
     id: 2,
     name: "IPhone 12",
     desc: "5.4-inch display",
     price: 600,
-    image: "./assets/images/iphone12.jpg",
+    image: "iphone12.jpg",
   },
   {
     id: 3,
     name: "Galaxy S",
     desc: "4K display resolation",
     price: 669,
-    image: "./assets/images/galaxyS.jpg",
+    image: "galaxyS.jpg",
   },
   {
     id: 4,
     name: "Poco Fi",
     desc: "Gaming CPU and Graphic",
     price: 999,
-    image: "./assets/images/pocof1.jpg",
+    image: "pocof1.jpg",
   },
 ];
 
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,7 +2,32 @@ import { useGetAllProductsQuery } from "../features/productsApi";
 
 const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
-  return <div className="container">Home Component</div>;
+  return (
+    <div className="container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occured</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div key={product.id} className="product">
+                <img src={ require(`../assets/images/${product.image}`) } />
+                <h3>{product.name}</h3>
+                <div className="details">
+                  <span>${product.price}</span>
+                  <span>{product.desc}</span>
+                </div>
+                <button>Add to cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
